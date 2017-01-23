7d8088a5006715c6e9d01d7fe3dd50f4d4f7116a
Steven Bell <stv.bell07@gmail.com>
2017-01-22 19:00:00 -0500

    Swapped out strcpy with strncpy call as required by collectd CI GCC Poison directive.

--- src/nut.c
+++ src/nut.c
@@ -130,7 +130,7 @@ static int nut_verify_peer(const char *value){
 static int nut_ca_path(const char *value){
   if (value != NULL && strcmp(value,"") !=0){
     ca_path = malloc(strlen(value)+1);
-    strcpy(ca_path, value);
+    strncpy(ca_path, value, (strlen(value)+1));
   }else{
     ca_path = NULL; // Should alread be set to NULL from initialization
   }
